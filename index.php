05cfb4b2c7ae72314d96c1303a041a442eff77a4
Iara de Amorim <iara-de-amorim@outlook.com>
2018-10-17 17:49:10 -0300

    Admin login/logout routes added

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 Use \Slim\Slim;
 Use \Hcode\Page;
 Use \Hcode\PageAdmin;
+Use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -18,11 +19,37 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
-    
+    User::verifyLogin();
 	$page = new PageAdmin;
 	$page->setTpl("index");
 
 });
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer" => false,
+		"title"=>"login"
+	]);
+	$page->setTpl("login");
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"],$_POST["password"]);
+	header("location: /admin");
+	exit;
+
+});
+
+$app->get("/admin/logout", function(){
+	User::logout();
+	header("location: /admin/login");
+	exit;
+});
+
+
 
 
 $app->run();
